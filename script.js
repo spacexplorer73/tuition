76c79750e70cf2650604ee81bbc24462c58c3f09
spacexplorer73 <77790064+spacexplorer73@users.noreply.github.com>
2021-03-01 17:09:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,8 +32,8 @@ function hideMarquee() {
   }
     
   var y = document.getElementById("nav-links");
-  if (y.style.display === "none") {
-    y.style.display = "block";
+  if (y.style.display === "block") {
+    y.style.display = "none";
   } else {
     y.style.display = "none";
   }
